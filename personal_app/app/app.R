85d5f8d8b4334f31308f75803caf8b210fd18046
e1155992ari <162811387+e1155992ari@users.noreply.github.com>
2024-04-07 22:09:38 +0800

    updated visualisation

--- personal_app/app/app.R
+++ personal_app/app/app.R
@@ -8,6 +8,7 @@
 #
 
 library(shiny)
+library(dplyr)
 library(ggplot2)
 dataset <- read.csv("Video Games Sales.csv")
 
@@ -15,11 +16,14 @@ dataset <- read.csv("Video Games Sales.csv")
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Global Sales of Video Game Genres over Time"),
+    titlePanel("Global Sales/Count of Video Game Genres over Time"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
+            selectInput(inputId = "select",
+                        label = "Select Y-Variable to be Examined:",
+                        choices = c("Global Sales", "Global Count")),
             sliderInput(inputId = "range",
                         label = "Range of Years Examined:",
                         min = 1983,
@@ -33,7 +37,8 @@ ui <- fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("line")
+           plotOutput("line"),
+           h5("This visualisation helps with examining, through a line graph, not only the trend of sales/number of copies sold for each video game genre, but also a comparison of sales/number of copies sold among genres against time.")
         )
     )
 )
@@ -45,17 +50,36 @@ server <- function(input, output) {
     filtered_data <- subset(dataset, Year >= input$range[1] & Year <= input$range[2])
     # Filter dataset based on selected genres
     filtered_data <- filtered_data[filtered_data$Genre %in% input$options, ]
-    # Create line plot
-    ggplot(data = filtered_data,
-           aes(x = Year, y = Global, colour = Genre)) +
-      geom_line() +
-      labs(title = "Global Sales of each Genre over Time",
-           x = "Year", 
-           y = "Global Sales",
-           colour = "Genre") +
-      theme_minimal()
+    
+    if (input$select == "Global Sales") {
+      # Create line plot for Global Sales
+      ggplot(data = filtered_data,
+             aes(x = Year, y = Global, colour = Genre)) +
+        geom_line() +
+        labs(title = "Global Sales of each Genre over Time",
+             x = "Year", 
+             y = "Global Sales",
+             colour = "Genre") +
+        theme_minimal()
+    } else if (input$select == "Global Count") {
+      # Create a new data frame with count of observations for each genre per year
+      count_data <- filtered_data %>%
+        group_by(Year, Genre) %>%
+        summarise(Count = n())
+      
+      # Create line plot for Global Count
+      ggplot(data = count_data,
+             aes(x = Year, y = Count, colour = Genre)) +
+        geom_line() +
+        labs(title = "Global Count of Copies Sold for Each Video Game Genre over Time",
+             x = "Year", 
+             y = "Count",
+             colour = "Genre") +
+        theme_minimal()
+    }
   })
 }
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
